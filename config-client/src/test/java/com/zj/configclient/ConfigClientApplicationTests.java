32afe9d160b0d0f07e708090618666a6b1843671
zhoujian <zj851112>
2020-09-25 21:50:17 +0800

    Initial commit

---
+++ config-client/src/test/java/com/zj/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zj.configclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/zj/scconfig/ScConfigApplication.java
@@ -0,0 +1,13 @@
+package com.zj.scconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScConfigApplication.class, args);
+	}
+
+}
