053dfd4f52224464d3000e2a33c6d7c230cb5901
zhouj <zj851112>
2020-09-27 16:57:45 +0800

    代码提交

--- config-client/src/main/java/com/zj/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/zj/configclient/ConfigClientApplication.java
@@ -3,22 +3,28 @@ package com.zj.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 
-	@Value("${foo}")
-	String foo;
+    @Value("${foo}")
+    String foo;
 
-	@RequestMapping("/hi")
-	public String hi(){
-	    return foo;
+    @RequestMapping("/hi")
+    public String hi() {
+        return foo;
     }
 }
